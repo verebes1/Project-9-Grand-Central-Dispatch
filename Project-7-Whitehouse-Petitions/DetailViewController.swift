b1c1c6fae115f1fbad0b8a4a03da2d7865feb5a5
David Verbenyi <dverbenyi@gmail.com>
2018-06-19 13:59:51 +0100

    Initial Commit

---
+++ Project-7-Whitehouse-Petitions/DetailViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DetailViewController.swift
+//  Project-7-Whitehouse-Petitions
+//
+//  Created by verebes on 19/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadDetailsInHTML()
+
+    }
+
+    func loadDetailsInHTML(){
+        guard detailItem != nil else {return}
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+    }
+
+}
---
+++ Project-7-Whitehouse-Petitions/TableViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  Project-7-Whitehouse-Petitions
+//
+//  Created by verebes on 19/06/2018.
+//  Copyright © 2018 A&D Progress. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+    var petitions = [[String: String]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        downloadPetitions()
+        
+    }
+    
+    //MARK:- Get petitions from Whitehouse
+    func downloadPetitions(){
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0{
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+
+        if let url = URL(string: urlString) {
+            if let data = try? String(contentsOf: url) {
+                let json = JSON(parseJSON: data)
+                
+                if json["metadata"]["responseInfo"]["status"].intValue == 200 {
+                    //we can parse the JSON
+                    parse(json)
+                    return
+                }
+            }
+        }
+        //the error message only shows if any of the conditions will fail and we will not get to parse and return
+        showError()
+    }
+    
+    func showError(){
+        let ac = UIAlertController(title: "Loading error", message: "There was an error loading the feed plese check your internet connection and try again later.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    //MARK:- JSON Parsing
+    
+    func parse(_ json: JSON){
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let body = result["body"].stringValue
+            let sigs = result["signatureCount"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            petitions.append(obj)
+        }
+        tableView.reloadData()
+    }
+
+    //MARK:- TableView Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition["title"]
+        cell.detailTextLabel?.text = petition["body"]
+        
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
+    }
+
+
+}
+
